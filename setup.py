8125407ff800aecf3f538d718949a21c9a6540e1
Eduard Carreras <ecarreras@gisce.net>
2015-11-06 13:19:25 +0100

    Bump to v0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='esios',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     url='https://github.com/gisce/esios',
     license='MIT',
