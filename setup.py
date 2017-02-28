b6eefeeabd1256a948a5fba5083cd12f9c59d9c6
tinogis <afita@gisce.net>
2017-02-28 12:45:55 +0100

    Bump to v0.1.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='esios',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
     url='https://github.com/gisce/esios',
     license='MIT',
