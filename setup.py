378fddf676f3503592d59ca7090356eebb93ef36
Xavi Torelló <info@xaviertorello.cat>
2017-09-19 11:59:11 +0200

    Update to 0.1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='esios',
-    version='0.1.9',
+    version='0.1.10',
     packages=find_packages(),
     url='https://github.com/gisce/esios',
     license='MIT',
