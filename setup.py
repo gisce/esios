1145d61f41b51783b6ee65a5da1c1c307ab61573
dmunoz <dmunoz@gisce.net>
2022-07-14 13:17:41 +0200

    Bump to v0.15.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGES_DATA = {'esios': ['data/*.xsd']}
 
 setup(
     name='esios',
-    version='0.14.0',
+    version='0.15.0',
     packages=find_packages(),
     url='https://github.com/gisce/esios',
     license='MIT',
