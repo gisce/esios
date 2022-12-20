b66b357bb9e4747bf16967ede2d566bb3d889314
Eduard Carreras <ecarreras@gisce.net>
2022-12-20 09:42:57 +0100

    Bump to v0.19.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGES_DATA = {'esios': ['data/*.xsd']}
 
 setup(
     name='esios',
-    version='0.18.0',
+    version='0.19.0',
     packages=find_packages(),
     url='https://github.com/gisce/esios',
     license='MIT',
