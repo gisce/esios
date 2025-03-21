8fa76b81efc698ba9aeef2e2b43bcbd7889e89ba
PSala <polsalalidon@gmail.com>
2025-03-21 12:33:03 +0100

    Bump to v0.24.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGES_DATA = {'esios': ['data/*.xsd']}
 
 setup(
     name='esios',
-    version='0.23.0',
+    version='0.24.0',
     packages=find_packages(),
     url='https://github.com/gisce/esios',
     license='MIT',
